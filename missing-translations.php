0be1d77ad4bbe2176a81724c1fd1134115024635
tillkruess <tillkruess@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-01 06:18:08 +0000

    more helper files shortened "do not translate" comment
    git-svn-id: https://plugins.svn.wordpress.org/humanstxt/branches/helpers@446029 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- missing-translations.php
+++ missing-translations.php
@@ -4,47 +4,16 @@
 		exit('wordpress.pot not found, download from: http://svn.automattic.com/wordpress-i18n/pot/trunk/wordpress.pot');
 	}
 
-	require dirname(__FILE__).'/../../../../wp-includes/pomo/po.php';
+	require dirname(__FILE__).'/strings.php';
+	require dirname(__FILE__).'/lib/po.php';
 
 	$po = new PO();
 	$po->import_from_file(dirname(__FILE__).'/wordpress.pot');
 
-	$strings = array(
-		// singular
-		'Cheatin&#8217; uh?',
-		'These revisions are identical.',
-		'Settings',
-		'Settings saved.',
-		'5 stars',
-		'4 stars',
-		'3 stars',
-		'2 stars',
-		'1 star',
-		'Save Changes',
-		'An unknown error occurred.',
-		'Save',
-		'These revisions are identical.',
-		'Older: %s',
-		'Newer: %s',
-		'Revisions',
-		'Compare Revisions',
-		'Author',
-		'Actions',
-		'%1$s [Current Revision]',
-		'Restore',
-		// plural
-		'(based on %s rating)',
-		// context
-		'revision date format'.chr(4).'j F, Y @ G:i',
-		'revisions column name'.chr(4).'Old',
-		'revisions column name'.chr(4).'New',
-		'revisions column name'.chr(4).'Date Created'
-	);
-
 	foreach ($strings as $string) {
 		if (!has_translation($string)) print 'missing translation for: '.$string."\n";
 	}
-	print "all good...\n";
+	print "\ndone...\n";
 
 	function has_translation($string) {
 		global $po;
---
+++ strings.php
@@ -0,0 +1,35 @@
+<?php
+
+// is there a "more generic" way?
+
+$strings = array(
+	// singular
+	'Cheatin&#8217; uh?',
+	'These revisions are identical.',
+	'Settings',
+	'Settings saved.',
+	'5 stars',
+	'4 stars',
+	'3 stars',
+	'2 stars',
+	'1 star',
+	'Save Changes',
+	'An unknown error occurred.',
+	'Save',
+	'These revisions are identical.',
+	'Older: %s',
+	'Newer: %s',
+	'Revisions',
+	'Compare Revisions',
+	'Author',
+	'Actions',
+	'%1$s [Current Revision]',
+	'Restore',
+	// plural
+	'(based on %s rating)',
+	// context
+	'revision date format'.chr(4).'j F, Y @ G:i',
+	'revisions column name'.chr(4).'Old',
+	'revisions column name'.chr(4).'New',
+	'revisions column name'.chr(4).'Date Created'
+);
