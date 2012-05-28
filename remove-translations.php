ee3751d07ba5eb991ec892139b62bc27ee06d858
tillkruess <tillkruess@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-28 03:29:26 +0000

    helpers: updated translation remov0r
    git-svn-id: https://plugins.svn.wordpress.org/humanstxt/branches/helpers@549645 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- remove-translations.php
+++ remove-translations.php
@@ -1,12 +1,8 @@
 <pre><?php
 
-	// make this thing more generic and use ./strings.php
-
 	if (!file_exists(dirname(__FILE__).'/humanstxt.pot')) {
 		exit('humanstxt.pot not found, download from: http://plugins.svn.wordpress.org/humanstxt/trunk/languages/humanstxt.pot');
 	}
-
-	require dirname(__FILE__).'/strings.php';
 
 	$pot = file_get_contents(dirname(__FILE__).'/humanstxt.pot');
 	$pot = preg_replace('~#\. translators: DO NOT TRANSLATE!.+?\n#~s', '#', $pot);
