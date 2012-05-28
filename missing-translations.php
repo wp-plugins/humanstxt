1d2fcefeb67333747b407ad3a7cd8bc7a1572732
tillkruess <tillkruess@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-28 03:19:05 +0000

    helpers: removed star-rating from string list helpers: added wordpress-admin.pot
    git-svn-id: https://plugins.svn.wordpress.org/humanstxt/branches/helpers@549642 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- missing-translations.php
+++ missing-translations.php
@@ -4,11 +4,16 @@
 		exit('wordpress.pot not found, download from: http://svn.automattic.com/wordpress-i18n/pot/trunk/wordpress.pot');
 	}
 
+	if (!file_exists(dirname(__FILE__).'/wordpress-admin.pot')) {
+		exit('wordpress.pot not found, download from: http://svn.automattic.com/wordpress-i18n/pot/trunk/wordpress-admin.pot');
+	}
+
 	require dirname(__FILE__).'/strings.php';
 	require dirname(__FILE__).'/lib/po.php';
 
 	$po = new PO();
 	$po->import_from_file(dirname(__FILE__).'/wordpress.pot');
+	$po->import_from_file(dirname(__FILE__).'/wordpress-admin.pot');
 
 	foreach ($strings as $string) {
 		if (!has_translation($string)) print 'missing translation for: '.$string."\n";
