716465e6f6573f1b0548124a0853a734eaf9899e
tillkruess <tillkruess@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-06 10:56:54 +0000

    syntax update
    git-svn-id: https://plugins.svn.wordpress.org/humanstxt/trunk@692659 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- legacy.php
+++ legacy.php
@@ -32,62 +32,62 @@
  * @param string $classes
  * @return string $classes 
  */
-function humanstxt_admin_body_class($classes) {
-	if (is_rtl() && strpos($classes, 'rtl') === false) {
+function humanstxt_admin_body_class( $classes ) {
+	if ( is_rtl() && strpos( $classes, 'rtl' ) === false ) {
 		$classes .= ' rtl ';
 	}
 	return $classes;
 }
-add_filter('admin_body_class', 'humanstxt_admin_body_class');
+add_filter( 'admin_body_class', 'humanstxt_admin_body_class' );
 
-if (!function_exists('esc_textarea')) :
+if ( !function_exists( 'esc_textarea' ) ) :
 /**
  * Escaping for textarea values introduced in WordPress 3.1.
  * Source: http://codex.wordpress.org/Function_Reference/esc_textarea
  */
-function esc_textarea($text) {
-	$safe_text = htmlspecialchars($text, ENT_QUOTES);
-	return apply_filters('esc_textarea', $safe_text, $text);
+function esc_textarea( $text ) {
+	$safe_text = htmlspecialchars( $text, ENT_QUOTES );
+	return apply_filters( 'esc_textarea', $safe_text, $text );
 }
 endif;
 
-if (!function_exists('str_ireplace')) :
+if ( !function_exists( 'str_ireplace' ) ) :
 /**
  * Case-insensitive version of str_replace() for PHP4.
  * Source: http://pear.php.net/package/PHP_Compat/
  */
-function str_ireplace($search, $replace, $subject) {
+function str_ireplace( $search, $replace, $subject ) {
 	// Sanity check
-	if (is_string($search) && is_array($replace)) {
-		user_error('Array to string conversion', E_USER_NOTICE);
+	if ( is_string( $search ) && is_array( $replace ) ) {
+		user_error( 'Array to string conversion', E_USER_NOTICE );
 		$replace = (string) $replace;
 	}
 
 	// If search isn't an array, make it one
 	$search = (array) $search;
-	$length_search = count($search);
+	$length_search = count( $search );
 
 	// build the replace array
-	$replace = is_array($replace)
-	? array_pad($replace, $length_search, '')
-	: array_pad(array(), $length_search, $replace);
+	$replace = is_array( $replace )
+	? array_pad( $replace, $length_search, '' )
+	: array_pad(array(), $length_search, $replace );
 
 	// If subject is not an array, make it one
 	$was_string = false;
-	if (is_string($subject)) {
+	if ( is_string( $subject ) ) {
 		$was_string = true;
-		$subject = array ($subject);
+		$subject = array( $subject );
 	}
 
 	// Prepare the search array
-	foreach ($search as $search_key => $search_value) {
-		$search[$search_key] = '/' . preg_quote($search_value, '/') . '/i';
+	foreach ( $search as $search_key => $search_value ) {
+		$search[ $search_key ] = '/' . preg_quote( $search_value, '/' ) . '/i';
 	}
 	
 	// Prepare the replace array (escape backreferences)
-	$replace = str_replace(array('\\', '$'), array('\\\\', '\$'), $replace);
+	$replace = str_replace( array( '\\', '$' ), array( '\\\\', '\$' ), $replace );
 
-	$result = preg_replace($search, $replace, $subject);
+	$result = preg_replace( $search, $replace, $subject );
 	return $was_string ? $result[0] : $result;
 }
 endif;
